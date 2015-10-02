fca02395b2236b3d7c027ea763979651b30b8be3
RB <rb@f0w.org>
2015-10-02 15:08:58 +0300

    Added Storage

--- src/Lamantin/Client.php
+++ src/Lamantin/Client.php
@@ -18,12 +18,18 @@ class Client
     private $parser;
 
     /**
+     * @var Storage
+     */
+    private $storage;
+
+    /**
      * Constructor
      */
     public function __construct()
     {
         $this->request = new Request();
         $this->parser = new Parser(new Crawler(), new Normalizer());
+        $this->storage = new Storage(getenv('HOME') . '/.lamantin');
     }
 
     /**
@@ -33,8 +39,14 @@ class Client
      */
     public function getMenu()
     {
+        $file_name = 'menu_' . date('Y-m-d');
+        if ($this->storage->has($file_name)) {
+            return $this->storage->get($file_name);
+        }
+
         $cafe_list = $this->parser->parseCafeList($this->request->request(self::BASE_LAMANTIN_URL)->getContent());
         $menu_map = $this->parseAllCafeMenu($cafe_list);
+        $this->storage->put($file_name, $menu_map);
 
         return $menu_map;
     }
